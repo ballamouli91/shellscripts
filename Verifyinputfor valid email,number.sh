366b7a86539053bcf93280381bbcd6f2fb5a6e6a
ballamouli91 <49180685+ballamouli91@users.noreply.github.com>
2019-10-24 16:54:04 +0530

    Update and rename Verifyinputfor valid email,number to Verifyinputfor valid email,number.sh

--- Verifyinputfor valid email,number.sh
+++ Verifyinputfor valid email,number.sh
@@ -1,3 +1,11 @@
+# ProblemStatement:
+#Create a Shell script file
+#Take user input for email id, phone and bank account number
+#Sanitize and trim the input taken
+#Check the validity of the input. i.e. valid email, 10 digit phone number and 5 digit account number
+#Display ‘All inputs valid’, else ‘Some inputs are invalid’
+
+
 if [[ "$1" =~ ^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$ ]]
 then
     echo "Email address $1 is valid."
