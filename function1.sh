3eddcca57a87ba2a6928cb01ad74696345c2be7b
ballamouli91 <49180685+ballamouli91@users.noreply.github.com>
2020-02-05 18:40:21 +0530

    Create function1.sh

---
+++ function1.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+# A funcation to draw a line
+drawline () {
+declare line=""
+declare char="="
+for (( i=1; i<=$1; ++i )); do
+line="${line}${char}"
+done
+printf "%s\n" "$line"
+}
+
+#no arguments? no output
+[[ ! $1 ]] && exit 0
+declare -i len="${#1} + 4 "
+drawline len
+printf "| %s |\n" "$1"
+drawline len
