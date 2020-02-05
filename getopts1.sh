c8ef5b7b63b0f03320491decb7a229cb9b69e4d1
ballamouli91 <49180685+ballamouli91@users.noreply.github.com>
2020-02-05 15:47:32 +0530

    Update getopts1.sh
    usage bash getopts1.sh -b -r -g  -c GREEN bye
    **********output will be like 
    Color is GREEN
    6
    Remaining arguments are:   bye

--- getopts1.sh
+++ getopts1.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+
 while getopts "brgc:" OPTION; do
+
     case $OPTION in
     b)
         COLOR=BLUE
@@ -26,4 +28,13 @@ while getopts "brgc:" OPTION; do
 done
 
 echo "Color is $COLOR"
+# When the script is invoked, the $OPTIND variable is set to 1, and it is incremented each time an option is parsed, until it reaches the last one. 
+# CONSIDERING THE OPTIONS AS (-b -r -g  -c GREEN ji ) Therefore, in this case, it will have a value of 6 when it will reach the last element, which is GREEN, the mandatory argument required by the -a option.
+echo "original options $OPTIND "
+#shift is a shell builtin which moves the positional parameters of the script down a specified number of positions provided to it as a positive number, discarding the related arguments.
+shift $(($OPTIND - 1))
+echo "left out options are : $# "
+# The way we launched the script above, the last provided argument, "ji", is not parsed by getopts, which at that point already finished its job.
+#Now, if we make a shift of the value of $OPTIND - 1 (that is the effective number of the parsed options, 5 in this case) on the positional parameters, what remains are just the arguments that are not options, in this case "bye", that we can access by using the $* variable later in this script.
+printf "Remaining arguments are: %s  $*"
 exit 0;
