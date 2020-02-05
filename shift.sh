a9981be0efee483cf8d006c5ab0fb9e6e757c5b4
ballamouli91 <49180685+ballamouli91@users.noreply.github.com>
2020-02-05 12:46:25 +0530

    Create shift.sh

---
+++ shift.sh
@@ -0,0 +1,8 @@
+
+#! /bin/bash
+#demo for shift option in script and see heow postion of parameters are changed
+HOST=$1
+USERNAME=$2
+shift
+PASSWORD=$3
+echo "Host is $HOST ; Username is $USERNAME ; Password is $PASSWORD"
