dd69f0ee817c4f0b3f94852b670b1f3f3613d64d
ballamouli91 <49180685+ballamouli91@users.noreply.github.com>
2020-02-07 14:02:26 +0530

    Update create_dated_copy_file_with_arg_passed.sh

--- activities_ryanstutorials.net/1/create_dated_copy_file_with_arg_passed.sh
+++ activities_ryanstutorials.net/1/create_dated_copy_file_with_arg_passed.sh
@@ -4,6 +4,6 @@
 echo " enter file name you wanted to create: "
 [[ $1 ]] || { echo “missing argument” >&2; exit 1; } 
 echo "$#"
-[[ "$#" -gt 1 ]]; { echo "need only one argument" >&2; exit 1; }
+[[ $# -ne 1 ]] && { echo "need only one argument " >&2; exit 1; }
 filename= $1
 touch "$(date)_$filename"
