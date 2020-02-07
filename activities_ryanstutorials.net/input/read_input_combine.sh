2cc4044b44c632805889024bebf09d9feb273cd3
ballamouli91 <49180685+ballamouli91@users.noreply.github.com>
2020-02-07 14:43:14 +0530

    Update read_input_combine.sh

--- activities_ryanstutorials.net/input/read_input_combine.sh
+++ activities_ryanstutorials.net/input/read_input_combine.sh
@@ -1,9 +1,11 @@
 # Create a simple script which will ask the user for a few pieces of information 
 # then combine this into a message which is echo'd to the screen.
-
+# add in some data coming from command line arguments and maybe some of the other system variables.
 #! /bin/bash
 read -p "please enteryour name: " name
 read -p " where are your from? " place
 read -p "where you going? " destination
 
 echo " my name is $name, I am from $place and am going to $destination"
+echo " my home location is $HOME; my system name is $HOSTNAME ; my system langauge is $LANG"
+echo " my default shell is $SHELL ; my terminal is $TERM; my editor is $EDITOR "
