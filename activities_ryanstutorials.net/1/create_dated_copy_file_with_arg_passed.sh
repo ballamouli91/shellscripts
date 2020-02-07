a1063dcdeae3803d48e52fbc791dd87f347676bf
ballamouli91 <49180685+ballamouli91@users.noreply.github.com>
2020-02-07 13:50:57 +0530

    Update create_dated_copy_file_with_arg_passed.sh

--- activities_ryanstutorials.net/1/create_dated_copy_file_with_arg_passed.sh
+++ activities_ryanstutorials.net/1/create_dated_copy_file_with_arg_passed.sh
@@ -1,5 +1,9 @@
+# create a script which will take a filename as its first argument and create a dated copy of the file. eg. if our file was named file1.txt it would create a copy such as 2020-02-06_file1.txt. (To achieve this you will probably want to play with command substitution and the command date
+
 #! /bin/bash
 echo " enter file name you wanted to create: "
-[[ "$#" -eq 0 ]] || [[ "$#" -gt 1 ]] ; echo " need only one argument"
-filename=$1
+[[ $1 ]] || { echo “missing argument” >&2; exit 1; } 
+echo "$#"
+[[ "$#" -gt 1 ]]; { echo "need only one argument" >&2; exit 1; }
+filename= $1
 touch "$(date)_$filename"
