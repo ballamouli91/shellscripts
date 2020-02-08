baf764fb44971570f8102b9adb963a90565c8bd4
ballamouli91 <49180685+ballamouli91@users.noreply.github.com>
2020-02-08 15:54:40 +0530

    Update create_random_number_0-99.sh

--- activities_ryanstutorials.net/arithmetic/create_random_number_0-99.sh
+++ activities_ryanstutorials.net/arithmetic/create_random_number_0-99.sh
@@ -12,3 +12,11 @@ echo $((RANDOM%10 + 1))
 echo $((RANDOM%100 + 1))
 
 echo ${RANDOM:0:2} # random number between 1 an 99
+
+##Modify it so that you can specify as a command line argument the upper limit of the random number.
+# Can you make it so that a lower limit can be specified also? eg. if I ran ./random.sh 10 45 it would only return random numbers between 10 and 45.
+
+#NOTE: a NUMBER1 divided by the a number2 the reminder )modulus % is always less than the number 2
+
+echo $(( RANDOM % (45 - 10 + 1 ) + 10 )) # number can be between 10 to 45
+echo $(( RANDOM % ($1 - $2 + 1  ) + $2 )) # argument 1 should be greater than argument 2
