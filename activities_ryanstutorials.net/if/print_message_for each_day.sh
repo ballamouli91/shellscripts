e651c4a260170936f0c496227ce981ab2fec95a7
ballamouli91 <49180685+ballamouli91@users.noreply.github.com>
2020-02-08 17:30:43 +0530

    Update print_message_for each_day.sh

--- activities_ryanstutorials.net/if/print_message_for each_day.sh
+++ activities_ryanstutorials.net/if/print_message_for each_day.sh
@@ -28,3 +28,5 @@ echo "Satuday , Friday night hangover "
 elif [[ $dow -eq 7 ]]; then 
 echo "lazzy sunday "
 fi
+
+# ref: https://www.howtogeek.com/410442/how-to-display-the-date-and-time-in-the-linux-terminal-and-use-it-in-bash-scripts/
