dc461fd723d22317832940742c5d9c124bfa4e22
ballamouli91 <49180685+ballamouli91@users.noreply.github.com>
2020-02-14 14:44:12 +0530

    Update print_num.sh

--- activities_ryanstutorials.net/loops/print_num.sh
+++ activities_ryanstutorials.net/loops/print_num.sh
@@ -1,1 +1,14 @@
+#! /bin/bash
+#Create a simple script which will print the numbers 1 - 10 (each on a separate line) and whether they are even or odd.
 
+for ((i=1;i<=10;i++)); do
+echo $i
+# let  d=$i%2
+if [[ $i -eq 1 ]]; then 
+echo 
+if [[ $i%2 -eq 0 ]]; then 
+echo " $i is even"
+else 
+echo "$i is odd"
+fi
+done
