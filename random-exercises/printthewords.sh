a54ba146733fecd2d016ddfd2eabde7ec4fec2fe
ballamouli91 <49180685+ballamouli91@users.noreply.github.com>
2020-02-15 12:49:09 +0530

    Create printthewords.sh

---
+++ random-exercises/printthewords.sh
@@ -0,0 +1,8 @@
+#Write a shell script that displays “man”,”bear”,”pig”,”dog”,”cat”,and “sheep” on the screen with each appearing on a separate line.
+# Try to do this in as few lines as possible.
+
+ANIMALS="man bear pig dog cat sheep"
+for ANIMAL in $ANIMALS
+  do
+    echo $ANIMAL
+  done
