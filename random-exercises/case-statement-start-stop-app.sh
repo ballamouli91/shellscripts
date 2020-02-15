61cc89d95d19a60ed1eca82e090ca1586d2bb68c
ballamouli91 <49180685+ballamouli91@users.noreply.github.com>
2020-02-15 16:26:37 +0530

    Update case-statement-start-stop-app.sh

--- random-exercises/case-statement-start-stop-app.sh
+++ random-exercises/case-statement-start-stop-app.sh
@@ -1,4 +1,5 @@
-# Created the start-up script for an application start and stop.
+# Created the start-up script for an application start and stop. it uses a logging function. Additionally, 
+# tag each syslog message with “randomly” and include process ID. Generate a 3 random numbers.
 
 #!/bin/bash
 INPUT=$1
@@ -18,3 +19,14 @@ if [ $? -eq '0']
      echo "Error input"
      ;;
 esac
+
+function logging()
+  {
+   MESSAGE=$@
+   SET_MESSAGE="Random Number is:$MESSAGE"
+   echo "$SET_MESSAGE"
+   logger -i -p user.info "$SET_MESSAGE"
+  }
+logging $RANDOM
+logging $RANDOM
+logging $RANDOM
